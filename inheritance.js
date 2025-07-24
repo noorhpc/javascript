89c346c8719075d8b01d75b6ef6754d67199199b
noorhpc <noorhpc5033@gmail.com>
2025-07-24 22:13:11 +0500

    Update inheritance.js

--- inheritance.js
+++ inheritance.js
@@ -1,1 +1,23 @@
 alert("hello");
+const info ={
+    emp_id: 1001,
+    name: 'ali',
+    age: 25,
+    salary:1500
+
+
+    display: function(){
+
+        console.log(`employ_id: ${this.emp_id},
+            Name: ${this.name},
+            Age: ${this.age},
+            Salary: ${this.salary}`
+        );
+
+    }
+    addsalary(amount){
+        this.salary += amount;
+        return this.salary;
+    }
+
+};
